4b271d6fc749d0a21da926fc7f07634d5a4229db
Akash_Badadani <akashsgbadadani@gmail.com>
2023-01-28 15:35:52 +0530

    other files

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 function validateFormgym()
 {
     var date1=document.forms["myForm"]["date1"].value;
-    var date2=document.forms["myForm"]["date2"].value;
+    
     var a;
-    if(date1==""&&date2==""){
+    if(date1==""){
         a=0;
     }
     else{
@@ -11,7 +11,7 @@ function validateFormgym()
     }
     if(a==1){
         alert("Booking Successfull");
-        window.location.href="../foodapp.html";
+        window.location.href="index.html";
     }
     else{
         alert("Please choose date");
@@ -20,14 +20,9 @@ function validateFormgym()
 function validateFormseminar()
 {
     var date1=document.forms["seminar"]["date1"].value;
-    var date2=document.forms["seminar"]["date2"].value;
-    var date3=document.forms["seminar"]["date3"].value;
-    var date4=document.forms["seminar"]["date4"].value;
-    var date5=document.forms["seminar"]["date5"].value;
-    var date6=document.forms["seminar"]["date6"].value;
-    var date7=document.forms["seminar"]["date7"].value;
+
     var a;
-    if(date1==""&&date2==""&&date3==""&&date4==""&&date5==""&&date6==""&&date7==""){
+    if(date1==""){
         a=0;
     }
     else{
@@ -36,7 +31,7 @@ function validateFormseminar()
     
     if(a==1){
         alert("Booking Successfull");
-        window.location.href="../foodapp.html";
+        window.location.href="index.html";
     }
     else{
         alert("Please choose date");
@@ -45,12 +40,9 @@ function validateFormseminar()
 function validateFormsports()
 {
     var date1=document.forms["sports"]["date1"].value;
-    var date2=document.forms["sports"]["date2"].value;
-    var date3=document.forms["sports"]["date3"].value;
-    var date4=document.forms["sports"]["date4"].value;
-    var date5=document.forms["sports"]["date5"].value;
+   
     var a;
-    if(date1==""&&date2==""&&date3==""&&date4==""&&date5==""){
+    if(date1==""){
         a=0;
     }
     else{
@@ -59,7 +51,7 @@ function validateFormsports()
     
     if(a==1){
         alert("Booking Successfull");
-        window.location.href="../foodapp.html";
+        window.location.href="index.html";
     }
     else{
         alert("Please choose date");
@@ -70,7 +62,7 @@ function validateFormcafeteria()
     var c1=document.forms["cafeteria"]["c1"].value;
     if(c1){
         alert("Booking Successfull");
-        window.location.href="../foodapp.html";
+        window.location.href="index.html";
     }
     else{
         alert("Please choose");
